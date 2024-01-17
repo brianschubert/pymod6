b8bad42a4b046685e7d457f176e506215cca673f
Brian Schubert <schubert.b@northeastern.edu>
2024-01-17 17:02:37 -0500

    Add RTOPTIONS and ATMOSPHERE specifications

--- src/pymod6/_input.py
+++ src/pymod6/_input.py
@@ -4,9 +4,10 @@ Input JSON file handling.
 
 from __future__ import annotations
 
+import enum
 import json
 import re
-from typing import Any, Final
+from typing import Any, Final, Literal
 
 import pydantic
 from pydantic import ConfigDict
@@ -17,11 +18,218 @@ _COMMENT_PATTERN: Final = re.compile(
 )
 
 
+@enum.unique
+class RTExecutionMode(enum.Enum):
+    RT_TRANSMITTANCE = "RT_TRANSMITTANCE"
+    RT_THERMAL_ONLY = "RT_THERMAL_ONLY"
+    RT_SOLAR_AND_THERMAL = "RT_SOLAR_AND_THERMAL"
+    RT_LUNAR_AND_THERMAL = "RT_LUNAR_AND_THERMAL"
+    RT_LUNAR_IRRADIANCE = "RT_LUNAR_IRRADIANCE"
+
+
+@enum.unique
+class RTAlgorithm(enum.Enum):
+    RT_MODTRAN = "RT_MODTRAN"
+    RT_CORRK_SLOW = "RT_CORRK_SLOW"
+    RT_CORRK_FAST = "RT_CORRK_FAST"
+    RT_MODTRAN_POLAR = "RT_MODTRAN_POLAR"
+    RT_LINE_BY_LINE = "RT_LINE_BY_LINE"
+
+
+@enum.unique
+class RTMultipleScattering(enum.Enum):
+    RT_NO_MULTIPLE_SCATTER = "RT_NO_MULTIPLE_SCATTER"
+    RT_DISORT = "RT_DISORT"
+    RT_DISORT_AT_OBS = "RT_DISORT_AT_OBS"
+    RT_ISAACS_2STREAM = "RT_ISAACS_2STREAM"
+    RT_ISAACS_2STREAM_AT_OBS = "RT_ISAACS_2STREAM_AT_OBS"
+    RT_ISAACS_SCALED = "RT_ISAACS_SCALED"
+    RT_ISAACS_SCALED_AT_OBS = "RT_ISAACS_SCALED_AT_OBS"
+
+
+@enum.unique
+class AtmosphereModel(enum.Enum):
+    ATM_CONSTANT = "ATM_CONSTANT"
+    ATM_TROPICAL = "ATM_TROPICAL"
+    ATM_MIDLAT_SUMMER = "ATM_MIDLAT_SUMMER"
+    ATM_MIDLAT_WINTER = "ATM_MIDLAT_WINTER"
+    ATM_SUBARC_SUMMER = "ATM_SUBARC_SUMMER"
+    ATM_SUBARC_WINTER = "ATM_SUBARC_WINTER"
+    ATM_US_STANDARD_1976 = "ATM_US_STANDARD_1976"
+    ATM_USER_ALT_PROFILE = "ATM_USER_ALT_PROFILE"
+    ATM_USER_PRESS_PROFILE = "ATM_USER_PRESS_PROFILE"
+
+
+class AtmosphereProfileType(enum.Enum):
+    PROF_USER_DEF = "PROF_USER_DEF"
+    PROF_ALTITUDE = "PROF_ALTITUDE"
+    PROF_PRESSURE = "PROF_PRESSURE"
+    PROF_TEMPERATURE = "PROF_TEMPERATURE"
+
+    PROF_H2O = "PROF_H2O"
+    PROF_WATER_VAPOR = "PROF_WATER_VAPOR"
+
+    PROF_CO2 = "PROF_CO2"
+    PROF_CARBON_DIOXIDE = "PROF_CARBON_DIOXIDE"
+
+    PROF_O3 = "PROF_O3"
+    PROF_OZONE = "PROF_OZONE"
+
+    PROF_N2O = "PROF_N2O"
+    PROF_NITROUS_OXIDE = "PROF_NITROUS_OXIDE"
+
+    PROF_CO = "PROF_CO"
+    PROF_CARBON_MONOXIDE = "PROF_CARBON_MONOXIDE"
+
+    PROF_CH4 = "PROF_CH4"
+    PROF_METHANE = "PROF_METHANE"
+
+    PROF_O2 = "PROF_O2"
+    PROF_OXYGEN = "PROF_OXYGEN"
+
+    PROF_NO = "PROF_NO"
+    PROF_NITRIC_OXIDE = "PROF_NITRIC_OXIDE"
+
+    PROF_SO2 = "PROF_SO2"
+    PROF_SULFUR_DIOXIDE = "PROF_SULFUR_DIOXIDE"
+    PROF_SULPHUR_DIOXIE = "PROF_SULPHUR_DIOXIE"
+
+    PROF_NO2 = "PROF_NO2"
+    PROF_NITROGEN_DIOXIDE = "PROF_NITROGEN_DIOXIDE"
+
+    PROF_NH3 = "PROF_NH3"
+    PROF_AMMONIA = "PROF_AMMONIA"
+
+    PROF_HNO3 = "PROF_HNO3"
+    PROF_NITRIC_ACID = "PROF_NITRIC_ACID"
+
+    PROF_CCl3F = "PROF_CCl3F"
+    PROF_F11 = "PROF_F11"
+    PROF_CFC11 = "PROF_CFC11"
+
+    PROF_CCl2F2 = "PROF_CCl2F2"
+    PROF_F12 = "PROF_F12"
+    PROF_CFC12 = "PROF_CFC12"
+
+    PROF_CClF3 = "PROF_CClF3"
+    PROF_CFC13 = "PROF_CFC13"
+    PROF_F13 = "PROF_F13"
+
+    PROF_CF4 = "PROF_CF4"
+    PROF_F14 = "PROF_F14"
+    PROF_CFC14 = "PROF_CFC14"
+
+    PROF_CHClF2 = "PROF_CHClF2"
+    PROF_F22 = "PROF_F22"
+    PROF_CFC22 = "PROF_CFC22"
+
+    PROF_C2Cl3F3 = "PROF_C2Cl3F3"
+    PROF_F113 = "PROF_F113"
+    PROF_CFC113 = "PROF_CFC113"
+
+    PROF_C2Cl2F4 = "PROF_C2Cl2F4"
+    PROF_F114 = "PROF_F114"
+    PROF_CFC114 = "PROF_CFC114"
+
+    PROF_C2ClF5 = "PROF_C2ClF5"
+    PROF_F115 = "PROF_F115"
+    PROF_CFC115 = "PROF_CFC115"
+
+    PROF_ClONO2 = "PROF_ClONO2"
+    PROF_HNO4 = "PROF_HNO4"
+    PROF_CHCl2F = "PROF_CHCl2F"
+    PROF_CCl4 = "PROF_CCl4"
+    PROF_N2O5 = "PROF_N2O5"
+    PROF_AHAZE = "PROF_AHAZE"
+    PROF_AHAZE2 = "PROF_AHAZE2"
+    PROF_AHAZE3 = "PROF_AHAZE3"
+    PROF_AHAZE4 = "PROF_AHAZE4"
+    PROF_EQLWCZ = "PROF_EQLWCZ"
+    PROF_RRATZ = "PROF_RRATZ"
+    PROF_IHA = "PROF_IHA"
+    PROF_ICLD = "PROF_ICLD"
+    PROF_IVUL = "PROF_IVUL"
+    PROF_ISEA = "PROF_ISEA"
+    PROF_ICHR = "PROF_ICHR"
+
+
+class AtmosphereProfileUnits(enum.Enum):
+    UNT_UNKNOWN = "UNT_UNKNOWN"
+    UNT_KILOMETERS = "UNT_KILOMETERS"
+    UNT_TKELVIN = "UNT_TKELVIN"
+    UNT_TCELSIUS = "UNT_TCELSIUS"
+    UNT_TDELTA_KELVIN = "UNT_TDELTA_KELVIN"
+    UNT_TDEWPOINT_KELVIN = "UNT_TDEWPOINT_KELVIN"
+    UNT_TDEWPOINT_CELSIUS = "UNT_TDEWPOINT_CELSIUS"
+    UNT_PMILLIBAR = "UNT_PMILLIBAR"
+    UNT_PATM = "UNT_PATM"
+    UNT_DPPMV = "UNT_DPPMV"
+    UNT_DMOL_PER_CM3 = "UNT_DMOL_PER_CM3"
+    UNT_DGRAM_PER_KG = "UNT_DGRAM_PER_KG"
+    UNT_DGRAM_PER_M3 = "UNT_DGRAM_PER_M3"
+    UNT_REL_HUMIDITY = "UNT_REL_HUMIDITY"
+
+
 class ModtranInput(TypedDict, total=False):
+    # noinspection PyTypedDict
+    __pydantic_config__ = ConfigDict(extra="forbid")
     NAME: str
     DESCRIPTION: str
     CASE: int
     CASE_TEMPLATE: int
+    RTOPTIONS: RTOptions
+    ATMOSPHERE: Atmosphere
+
+
+class RTOptions(TypedDict, total=False):
+    IEMSCT: RTExecutionMode
+    MODTRN: RTAlgorithm
+    LYMOLC: bool
+    T_BEST: bool
+    IMULT: RTMultipleScattering
+    DISALB: bool
+    NSTR: int
+    NLBL: int
+    SOLCON: float
+
+
+class Atmosphere(TypedDict, total=False):
+    MODEL: AtmosphereModel
+    M1: AtmosphereModel
+    M2: AtmosphereModel
+    M3: AtmosphereModel
+    M4: AtmosphereModel
+    M5: AtmosphereModel
+    M6: AtmosphereModel
+    M2_RHC: bool
+    MDEF: Literal[0, 1, 2]
+    HMODEL: str
+    NLAYERS: int
+    NPROF: int
+    PROFILES: object  # TODO
+    CO2MX: float
+    H2OSTR: float
+    H2OUNIT: Literal[" ", "+", "g", "a"]
+    H2OOPT: Literal[" "]  # not documented?
+    O3STR: float
+    O3UNIT: Literal[" ", "g", "a"]
+    C_PROF: Literal[0, 1, 2, 3, 4, 5, 6, 7]
+    S_UMIX: list[float]
+    S_XSEC: list[float]
+    S_TRACE: list[float]
+    AERRH: float
+    AYRANG: bool
+    AYRANGFL: str
+    E_MASS: float
+    AIRMWT: float
+
+
+class AtmosphereProfile(TypedDict, total=False):
+    TYPE: AtmosphereProfileType
+    UNITS: AtmosphereProfileUnits
+    UNAME: str
+    PROFILE: list[float]
+    PRO_MASK: list[Literal[-1, 0, 1, 2, 3, 4, 5, 6]]
 
 
 class Case(TypedDict, total=False):
