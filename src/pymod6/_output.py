8a58cfc38572418571267b0a108b1e95da30126b
Brian Schubert <schubert.b@northeastern.edu>
2024-01-17 19:26:49 -0500

    Add initial ACD file reading tools (text and binary)

---
+++ src/pymod6/_output.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import contextlib
+import pathlib
+from typing import BinaryIO, TextIO
+
+import numpy as np
+
+_AtmoCorrectDataDType = np.dtype(
+    [
+        ("freq", "<f4"),
+        ("los", "<i4"),
+        ("k_int", "<i4"),
+        ("k_weight", "<f4"),
+        ("sun_gnd_diffuse_transm", "<f4"),
+        ("sun_gnd_obs_direct_transm", "<f4"),
+        ("obs_gdn_embedded_dif_transm", "<f4"),
+        ("obs_gnd_direct_transm", "<f4"),
+        ("spherical_albedo", "<f4"),
+    ]
+)
+
+_AtmoCorrectDataFileDtype = np.dtype(
+    [
+        ("_delim1", "B", 4),
+        ("data", _AtmoCorrectDataDType),
+        ("_delim2", "B", 4),
+    ]
+)
+
+
+def read_acd_text(file: pathlib.Path | TextIO) -> np.ndarray:
+    return np.loadtxt(file, skiprows=5, dtype=_AtmoCorrectDataDType)
+
+
+def read_acd_binary(file: pathlib.Path | BinaryIO) -> np.ndarray:
+    if isinstance(file, BinaryIO):
+        cm = contextlib.nullcontext(file)
+    else:
+        cm = open(file, "rb")
+
+    with cm as fd:
+        buffer = fd.read()
+
+    contents = np.frombuffer(
+        buffer,
+        offset=_AtmoCorrectDataFileDtype.itemsize,
+        dtype=_AtmoCorrectDataFileDtype,
+    )
+
+    return contents["data"]
