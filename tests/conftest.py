b51cc4784032c09f3a3340b5ef3cc792da5f83ba
Brian Schubert <schubert.b@northeastern.edu>
2024-01-23 15:49:12 -0500

    Add version pre-check to executable fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,9 +15,16 @@ def modtran_env() -> pymod6._env.ModtranEnv:
 @pytest.fixture(scope="session")
 def modtran_exec(modtran_env) -> pymod6._exec.ModtranExecutable:
     mod_exec = pymod6._exec.ModtranExecutable(env=modtran_env)
+
+    # Check license is active.
     if (
         status := mod_exec.license_status()
     ) != "STAT_VALID MODTRAN license is activated.":
         pytest.skip(f"MODTRAN license not active: {status}")
+
+    # Check version is compatible.
+    version = mod_exec.version().split()[-1]
+    if version.split(".")[0] != "6":
+        pytest.skip(f"invalid MODTRAN version, expected version 6.*, found {version}")
 
     return mod_exec
