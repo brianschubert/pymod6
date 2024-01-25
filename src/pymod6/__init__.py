a34491a639ae55c6ac10616893275881c8e0766c
Brian Schubert <schubert.b@northeastern.edu>
2024-01-24 19:17:09 -0500

    Update top module exports

--- src/pymod6/__init__.py
+++ src/pymod6/__init__.py
@@ -3,6 +3,12 @@ from __future__ import annotations
 import importlib.metadata
 from typing import Final
 
+from . import input, unit
+from ._env import ModtranEnv
+from ._exec import ModtranExecutable
+
 DISTRIBUTION_NAME: Final[str] = "pymod6"
 
 __version__ = importlib.metadata.version(DISTRIBUTION_NAME)
+
+__all__ = ["ModtranEnv", "ModtranExecutable"]
