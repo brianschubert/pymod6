9adb1db55cf5654c2de58ccf4aa3e76ad5f50824
Brian Schubert <schubert.b@northeastern.edu>
2024-01-24 19:15:58 -0500

    Add test cast comparing text and binary acd output

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import pymod6._exec
 import pymod6._output
 import pymod6.input as mod_input
@@ -126,3 +128,36 @@ def test_files_exist_json(modtran_exec, tmp_path) -> None:
                 mod_input.JSONPrintOpt.WRT_INPUT in json_opt,
                 mod_input.JSONPrintOpt.WRT_OUTPUT in json_opt,
             )
+
+
+def test_acd_text_binary_match(modtran_exec, tmp_path) -> None:
+    input_acd_text = (
+        mod_input.ModtranInputBuilder()
+        .add_case(mod_input.basecases.VNIR)
+        .finish_case()
+        .build_json_input(output_legacy=True, binary=False)
+    )
+    input_acd_binary = (
+        mod_input.ModtranInputBuilder()
+        .add_case(mod_input.basecases.VNIR)
+        .finish_case()
+        .build_json_input(output_legacy=True, binary=True)
+    )
+
+    acd_text = pymod6._output.read_acd_text(
+        modtran_exec.run(input_acd_text, work_dir=tmp_path)
+        .cases_output_files[0]
+        .acd_text,
+    )
+
+    acd_binary = pymod6._output.read_acd_binary(
+        modtran_exec.run(input_acd_binary, work_dir=tmp_path)
+        .cases_output_files[0]
+        .acd_binary,
+        check=True,
+    )
+
+    for field_name in acd_binary.dtype.names:
+        np.testing.assert_allclose(
+            acd_text[field_name], acd_binary[field_name], atol=5e-6, rtol=1e-6
+        )
