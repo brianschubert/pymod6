dce3e9fa8e6ca76a7872d9bfb9ca2dd9502391c8
Brian Schubert <schubert.b@northeastern.edu>
2024-01-28 13:50:26 -0500

    Refactor test modules

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,22 @@
+import copy
+
 import pytest
 
-import pymod6._env
-import pymod6._exec
+import pymod6
+from pymod6 import input as mod_input
 
 
 @pytest.fixture(scope="session")
-def modtran_env() -> pymod6._env.ModtranEnv:
+def modtran_env() -> pymod6.ModtranEnv:
     try:
-        return pymod6._env.ModtranEnv.from_environ()
+        return pymod6.ModtranEnv.from_environ()
     except ValueError as ex:
         pytest.skip(f"unable to detecte MODTRAN environment: {ex}")
 
 
 @pytest.fixture(scope="session")
-def modtran_exec(modtran_env) -> pymod6._exec.ModtranExecutable:
-    mod_exec = pymod6._exec.ModtranExecutable(env=modtran_env)
+def modtran_exec(modtran_env) -> pymod6.ModtranExecutable:
+    mod_exec = pymod6.ModtranExecutable(env=modtran_env)
 
     # Check license is active.
     if not (status := mod_exec.license_status()).startswith("STAT_VALID"):
@@ -26,3 +28,11 @@ def modtran_exec(modtran_env) -> pymod6._exec.ModtranExecutable:
         pytest.skip(f"invalid MODTRAN version, expected version 6.*, found {version}")
 
     return mod_exec
+
+
+@pytest.fixture(scope="session")
+def simple_case() -> mod_input.ModtranInput:
+    case: mod_input.ModtranInput = copy.deepcopy(mod_input.basecases.BASE)
+    case["SPECTRAL"]["V1"] = 4000
+    case["SPECTRAL"]["V2"] = 4001
+    return case
