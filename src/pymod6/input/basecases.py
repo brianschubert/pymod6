b8554bc934283a7039a212133573a43c9b98e8d4
Brian Schubert <schubert.b@northeastern.edu>
2024-01-28 17:22:05 -0500

    Add basecases module documentation

--- src/pymod6/input/basecases.py
+++ src/pymod6/input/basecases.py
@@ -1,3 +1,15 @@
+"""
+Prototypes for common input cases.
+
+.. warning::
+    Be careful not accidentally mutate these base case. Always make a *deep copy* of a base case before modifying it:
+
+    >>> import copy
+    >>> from pymod6.input.basecases import VNIR_SWIR
+    >>> my_base_case = copy.deepcopy(VNIR_SWIR)
+    >>> my_base_case[...] = ...
+"""
+
 import copy
 from typing import Final
 
@@ -23,20 +35,26 @@ BASE: Final[_json.ModtranInput] = _json.ModtranInput(
         BMNAME="01_2013",
     ),
 )
+"""Common base case."""
 
 # https://en.wikipedia.org/wiki/VNIR
 VNIR: Final[_json.ModtranInput] = copy.deepcopy(BASE)
+"""Visible and near-infrared (VNIR), 400-1400nm."""
 VNIR["SPECTRAL"]["V1"] = _unit.Wavelength(1400, "nm").as_wavenumber("cm-1")
 VNIR["SPECTRAL"]["V2"] = _unit.Wavelength(400, "nm").as_wavenumber("cm-1")
 
+
 SWIR: Final[_json.ModtranInput] = copy.deepcopy(BASE)
+"""Short-wavelength infrared (SWIR), 1400-2500nm."""
 SWIR["SPECTRAL"]["V1"] = _unit.Wavelength(2500, "nm").as_wavenumber("cm-1")
 SWIR["SPECTRAL"]["V2"] = _unit.Wavelength(1400, "nm").as_wavenumber("cm-1")
 
 VNIR_SWIR: Final[_json.ModtranInput] = copy.deepcopy(BASE)
+"""VNIR-SWIR, 400-2500nm."""
 VNIR_SWIR["SPECTRAL"]["V1"] = SWIR["SPECTRAL"]["V1"]
 VNIR_SWIR["SPECTRAL"]["V2"] = VNIR["SPECTRAL"]["V2"]
 
 LWIR: Final[_json.ModtranInput] = copy.deepcopy(BASE)
+"""Long-wavelength infrared (LWIR), 8-14um."""
 LWIR["SPECTRAL"]["V1"] = _unit.Wavelength(14, "um").as_wavenumber("cm-1")
 LWIR["SPECTRAL"]["V2"] = _unit.Wavelength(8, "um").as_wavenumber("cm-1")
