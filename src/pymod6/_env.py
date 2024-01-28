189b9e916a6627e75e632f380137dd6db9b60457
Brian Schubert <schubert.b@northeastern.edu>
2024-01-27 21:45:02 -0500

    Add documentation for env module

--- src/pymod6/_env.py
+++ src/pymod6/_env.py
@@ -22,15 +22,56 @@ _ENV_EXPORT_PATTERN: Final = re.compile(
 
 @dataclass
 class ModtranEnv:
+    """
+    MODTRAN environment.
+
+    Encapsulates environment variables relevant to MODTRAN. Typically, this class
+    will be constructed automatically from the actual process environment variables
+    via ``ModtranEnv.from_environ``.
+
+    When the relevant environment variables are not already set, this class can be
+    constructed manually to specify the environemnt MODTRAN should be run in.
+
+    Examples
+    --------
+
+    >>> from pathlib import Path
+    >>> mod_env = ModtranEnv(
+    ...     exe=Path("~/path/to/executable").expanduser(),
+    ...     data=Path("~/path/to/DATA").expanduser(),
+    ... )
+    >>> str(mod_env.exe)  # doctest: +ELLIPSIS
+    '.../path/to/executable'
+    >>> str(mod_env.data)
+    '.../path/to/DATA'
+    """
+
     exe: pathlib.Path
+    """Path to MODTRAN executable."""
+
     data: pathlib.Path
+    """Path to MODTRAN data directory."""
+
     extra: dict[str, str] = dataclasses.field(default_factory=dict)
+    """Extra environment variables to be made available to MODTRAN."""
 
     @classmethod
     def from_environ(cls, environ: Mapping[str, str] = os.environ) -> Self:
         """
         Infer MODTRAN environment from environment variables.
 
+        Parameters
+        ----------
+        environ : Mapping[str, str], optional
+            Mapping of environment variables. Defaults to ``os.environ``.
+
+        Returns
+        -------
+        Self
+            Inferred MODTRAN environment.
+
+        Examples
+        --------
         >>> ModtranEnv.from_environ(
         ...     {"MODTRAN_EXE": "exe", "MODTRAN_DATA": "data", "MODTRAN_OTHER": "other", "EXTRANEOUS": "misc"}
         ... ) # doctest: +ELLIPSIS
@@ -52,6 +93,18 @@ class ModtranEnv:
 
     @classmethod
     def from_shell_file(cls, file: str | pathlib.Path | TextIO) -> Self:
+        """
+        Infer MODTRAN environment by parsing a shell file.
+        Parameters
+        ----------
+        file : file path or file-like object
+            The shell file to extract environment variable definitions from.
+
+        Returns
+        -------
+        Self
+            Inferred MODTRAN environment.
+        """
         cm: ContextManager[TextIO]
         if _util.is_text_io(file):
             cm = contextlib.nullcontext(file)
@@ -69,6 +122,14 @@ class ModtranEnv:
         return cls.from_environ(env_exports)
 
     def to_environ(self) -> dict[str, str]:
+        """
+        Export this environment to an environment variable mapping.
+
+        Returns
+        -------
+        dict[str, str]
+            Environment variable mapping, like ``os.environ``.
+        """
         return {
             "MODTRAN_EXE": str(self.exe),
             "MODTRAN_DATA": str(self.data),
