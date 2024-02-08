67dfba636c0716161dded41b55e471f07a12858d
Brian Schubert <schubert.b@northeastern.edu>
2024-02-07 20:32:00 -0500

    Add nested mapping merging utility

--- src/pymod6/_util.py
+++ src/pymod6/_util.py
@@ -2,12 +2,40 @@ from __future__ import annotations
 
 import io
 import math
-from typing import Any, BinaryIO, MutableMapping, Sequence, TextIO, TypeVar
+from typing import (
+    Any,
+    BinaryIO,
+    Mapping,
+    MutableMapping,
+    Sequence,
+    TextIO,
+    TypeVar,
+    Union,
+    cast,
+)
 
-from typing_extensions import TypeGuard
+from typing_extensions import TypeAlias, TypeGuard
 
 _K = TypeVar("_K")
 _V = TypeVar("_V")
+
+_NestedMapping: TypeAlias = Mapping[_K, Union[_V, "_NestedMapping[_K, _V]"]]
+_NestedMutableMapping: TypeAlias = MutableMapping[
+    _K, Union[_V, "_NestedMutableMapping[_K, _V]"]
+]
+
+
+class MappingMergeError(ValueError):
+    """Raised when merging nested maps fails due to a key collision."""
+
+    key_path: tuple[Any, ...]
+
+    def __init__(self, *args: Any, key_path: tuple[Any, ...]) -> None:
+        self.key_path = key_path
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return f"collision at key {self.key_path}: {super().__str__()}"
 
 
 def is_binary_io(obj: Any) -> TypeGuard[BinaryIO]:
@@ -19,11 +47,23 @@ def is_text_io(obj: Any) -> TypeGuard[TextIO]:
 
 
 def assign_nested_mapping(
-    mapping: MutableMapping[_K, MutableMapping[_K, _V] | _V],
+    mapping: _NestedMutableMapping[_K, _V],
     keys: Sequence[_K],
     value: _V,
 ) -> None:
     """
+    Set a nested mapping item to the given value.
+
+    Calling
+    ```python
+    assign_nested_mapping(obj, (k1, k2, k3), value)
+    ```
+    is the same as writing
+    ```python
+    cls = obj.__class__
+    obj.setdefault(k1, cls()).setdefault(k2, cls()).setdefault(k3, cls()) = value
+    ```
+
     >>> x = {}; assign_nested_mapping(x, ("a", "b", "c"), 1); x
     {'a': {'b': {'c': 1}}}
     >>> x = {"a": {}}; assign_nested_mapping(x, ("a", "b", "c"), 1); x
@@ -33,16 +73,102 @@ def assign_nested_mapping(
     ...
     ValueError: expected mapping at (..., 'b'), found <class 'int'>
     """
-    curr: MutableMapping[_K, MutableMapping[_K, _V] | _V] = mapping
+    curr: _NestedMutableMapping[_K, _V] = mapping
+    cls = mapping.__class__
+
     for k in keys[:-1]:
         try:
-            curr.setdefault(k, mapping.__class__())  # type: ignore[arg-type]
+            curr.setdefault(k, cls())
         except AttributeError as ex:
             raise ValueError(
                 f"expected mapping at (..., '{k}'), found {type(curr)}"
             ) from ex
-        curr = curr[k]  # type: ignore[assignment]
+        curr = cast("_NestedMutableMapping[_K, _V]", curr[k])
     curr[keys[-1]] = value
+
+
+def merge_nested_mappings(
+    target: _NestedMutableMapping[_K, _V],
+    *sources: _NestedMapping[_K, _V],
+    allow_override: bool = False,
+) -> None:
+    """
+    Merge nested mappings into a single mapping.
+
+    After calling this funciton, the value of
+    ```
+    target[k1][k2][k3]
+    ```
+    will be equal to one of the following:
+    ```
+    sources[0][k1][k2][k3]
+    sources[1][k1][k2][k3]
+    # ...
+    sources[-2][k1][k2][k3]
+    sources[-1][k1][k2][k3]
+    ```
+    If `allow_override` is `False`, then only one of the expressions above may
+    successully evaluate to a value, and the rest must raise a `KeyError`.
+    The value of `target[k1][k2][k3]` is the single possible value.
+
+    If `allow_override` is `False`, then multiple of the expressions above may
+    successfully evaluate to a value. The value of `target[k1][k2][k3]` will be equal
+    to the *last* possible value.
+
+    Examples
+    --------
+
+    >>> x = {}
+    >>> merge_nested_mappings(x, {'color': 'red'}); x
+    {'color': 'red'}
+    >>> merge_nested_mappings(x, {'color': 'blue'}); x
+    Traceback (most recent call last):
+    ...
+    pymod6._util.MappingMergeError: collision at key ('color',): cannot override value when allow_override=False
+    >>> merge_nested_mappings(x, {'color': 'blue'}, allow_override=True); x
+    {'color': 'blue'}
+    >>> merge_nested_mappings(x, {"legs": {"cat": 4}}, {"legs": {"spider": 8}}); x
+    {'color': 'blue', 'legs': {'cat': 4, 'spider': 8}}
+
+    Mappings will be deeply copied, while all other values will be assigned directly:
+    >>> m = {"a": {"b": [1, 2, 3]}}
+    >>> x = {}
+    >>> merge_nested_mappings(x, m)
+    >>> x["a"] is m["a"]
+    False
+    >>> x["a"]["b"] is m["a"]["b"]
+    True
+    """
+    for source_map in sources:
+        for key, value in source_map.items():
+            key_defined = key in target
+
+            # Check for collision
+            if (
+                not allow_override
+                and key_defined
+                and not isinstance(target[key], Mapping)
+            ):
+                raise MappingMergeError(
+                    f"cannot override value when allow_override=False",
+                    key_path=(key,),
+                )
+
+            # Merge 'value' into `target[key]'.
+            if isinstance(value, Mapping):
+                if not key_defined:
+                    target[key] = target.__class__()
+                try:
+                    merge_nested_mappings(
+                        cast("_NestedMutableMapping[_K, _V]", target[key]),
+                        cast("_NestedMapping[_K, _V]", value),
+                        allow_override=allow_override,
+                    )
+                except MappingMergeError as ex:
+                    ex.key_path = (key, *ex.key_path)
+                    raise
+            else:
+                target[key] = cast("_V", value)
 
 
 def num_digits(x: int) -> int:
