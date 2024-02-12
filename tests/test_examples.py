38a037fc318366a788859853c4c71c2975832084
Brian Schubert <schubert.b@northeastern.edu>
2024-02-11 22:06:21 -0500

    Pre-fail example tests with missing dependencies, copy .rng files to work dir

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,6 +3,7 @@ Tests for examples in `<install root>/TEST/JSON`.
 """
 import os
 import pathlib
+import shutil
 from typing import TYPE_CHECKING
 
 import pytest
@@ -23,17 +24,55 @@ def example_json(example_file) -> pymod6.input.schema.JSONInput:
 
 @pytest.fixture(scope="module")
 def example_outputs(
-    modtran_exec, example_file, tmp_path_factory, helpers
+    modtran_exec, example_file, example_json, tmp_path_factory, helpers
 ) -> pymod6.output.ModtranOutputFiles:
-    if os.environ.get("TEST_EXAMPLE") != "1":
+    if os.environ.get("TEST_EXAMPLES") != "1":
         pytest.skip("set TEST_EXAMPLE=1 in the environment to run examples")
 
-    input_json = pymod6.io.read_json_input(example_file.read_text())
+    # TODO diagnosis why this example fails.
+    # Suppress for now, since its failure produces massive stdout.
+    if example_file.stem == "DISIsaacs":
+        pytest.skip(
+            "DISIsaacs example consistently errors with"
+            " 'Finite spectral bin equivalent width expansion did not converge'"
+        )
+
+    # Pre-fail examples that depend on 'DATA/Landsat8.flt' when missing.
+    if (
+        example_json["MODTRAN"][0]
+        .get("MODTRANINPUT", {})
+        .get("SPECTRAL", {})
+        .get("FILTNM")
+        == "DATA/Landsat8.flt"
+        and not modtran_exec._env.data.joinpath("DATA/Landsat8.flt").exists()
+    ):
+        pytest.fail("missing DATA/Landsat8.flt")
 
     run_dir = tmp_path_factory.mktemp("modtran_run")
 
+    # Copy corresponding .rng file to work dir if it exists.
+    rng_file = example_file.with_suffix(".rng")
+    if rng_file.exists():
+        shutil.copy(rng_file, run_dir)
+
+    # Pre-fail examples that are missing a required .rng file.
+    if not rng_file.exists() and any(
+        "~" in p.get("UNAME", "")
+        for p in example_json["MODTRAN"][0]["MODTRANINPUT"]["ATMOSPHERE"].get(
+            "PROFILES", []
+        )
+    ):
+        pytest.fail(
+            f"missing .rng file '{rng_file}' despite UNAME with '~' in ATMOSPHERE.PROFILE"
+        )
+
+    # Copy corresponding .OUT file to work dir if it exists.
+    out_file = example_file.with_suffix(".OUT")
+    if out_file.exists():
+        shutil.copy(out_file, run_dir)
+
     # Run example.
-    result = modtran_exec.run(input_json, work_dir=run_dir)
+    result = modtran_exec.run(example_json, work_dir=run_dir)
     assert result.process.returncode == 0
     assert "Error" not in result.process.stdout
     assert "" == result.process.stderr
